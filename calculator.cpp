8d1d6c96326a81b71cbcce362441f98a6ee1891e
Bekoff257 <108531510+Bekoff257@users.noreply.github.com>
2022-07-03 21:41:53 +0500

    Update and rename .gitignore to calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a, b;
+  cin >> a >> b;
+  cout << "1-Sonni kiriting"; cin >> a;
+  cout << "2-Sonni kiriting"; cin >> b;
+  cout << "Javob = " << a + b; //yoki (/, * , -)
+}
